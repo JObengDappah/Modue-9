d9083bcd5e3f46096aca4f08f1ae597aff07c0d9
JObengDappah <134343671+JObengDappah@users.noreply.github.com>
2023-10-12 19:47:19 -0400

    Markdown code added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,80 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
+// Function to render the license badge
+function renderLicenseBadge(license) {
+    if (license) {
+      // You can create an object that maps the license names to their respective badge URLs.
+      const licenseBadges = {
+        MIT: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+        'Apache 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+        // Add badges for other licenses here
+      };
+      return `[![License](${licenseBadges[license]})](${renderLicenseLink(license)})`;
+    }
+    return '';
+  }
+  
+  // Function to render the license link
+  function renderLicenseLink(license) {
+    if (license) {
+      // You can provide the URL where the full license text can be found.
+      const licenseLinks = {
+        MIT: 'https://opensource.org/licenses/MIT',
+        'Apache 2.0': 'https://www.apache.org/licenses/LICENSE-2.0',
+        // Add links for other licenses here
+      };
+      return licenseLinks[license];
+    }
+    return '';
+  }
+  
+  // Function to render the license section of README
+  function renderLicenseSection(license) {
+    if (license) {
+      return `## License
+  This application is covered under the [${license} license](${renderLicenseLink(license)}).
+  `;
+    }
+    return '';
+  }
+  
+  // Function to generate markdown for README
+  function generateMarkdown(data) {
+    const licenseBadge = renderLicenseBadge(data.license);
+    const licenseSection = renderLicenseSection(data.license);
+  
+    return `# ${data.title}
+  
+  ${licenseBadge}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${licenseSection}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ${licenseSection}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  For additional questions, please contact [${data.name}](https://github.com/${data.name}) at ${data.email}.
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
